70511923f3c5a256881fd3174d02bdf7f50857df
fede <cuentad1f3r3nt3@gmail.com.ar>
2025-05-26 15:19:35 -0300

    funciones

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,50 @@
+def suma(resultado):
+    print("")
+    num=float(input("Ingrese un número para sumar al anterior: "))
+    resultado += num
+    return resultado
+
+def resta(resultado):
+    print("")
+    num=float(input("Ingrese un número para restar al anterior: "))
+    resultado -= num
+    return resultado
+
+def multiplicacion(resultado):
+    print("")
+    num=float(input("Ingrese un número para multiplicar al anterior: "))
+    resultado *= num
+    return resultado
+
+def division(resultado):
+    print("")
+    num=float(input("Ingrese un número para dividir al anterior: "))
+    resultado /= num
+    return resultado
+
+def potencia(resultado):
+    print("")
+    num=int(input("Ingrese un número natural para potenciar al anterior: "))
+    resultado2 = resultado
+    if num == 0:
+        resultado2 = 1
+    else:
+        for i in range (num-1):
+            resultado2=resultado2*resultado
+    return resultado2
+
+def raiz(resultado):
+    print("")
+    raiz=int(input("Ingrese raíz: "))
+    while (raiz <= 0):
+        print("")
+        raiz=int(input("ERROR! Ingrese raíz válida: "))
+    if (raiz%2==0 and resultado<0):
+        print("ERROR! Esta cálculo es inválido matemáticamente")
+    else:
+        resultado = resultado ** (1/raiz)
+    return resultado
+
 def main():
 
     print("calculadora")
@@ -26,40 +73,17 @@ def main():
         selector = int(input("ERROR! Ingrese un valor válido(1-6): "))
     
     if (selector==1):
-        print("")
-        num=float(input("Ingrese un número para sumar al anterior: "))
-        resultado += num
+        resultado=suma(resultado)
     if (selector==2):
-        print("")
-        num=float(input("Ingrese un número para restar al anterior: "))
-        resultado -= num
+        resultado=resta(resultado)
     if (selector==3):
-        print("")
-        num=float(input("Ingrese un número para multiplicar al anterior: "))
-        resultado *= num
+        resultado=multiplicacion(resultado)
     if (selector==4):
-        print("")
-        num=float(input("Ingrese un número para dividir al anterior: "))
-        resultado /= num
+        resultado=division(resultado)
     if (selector==5):
-        print("")
-        num=int(input("Ingrese un número entero para potenciar al anterior: "))
-        resultado2 = resultado
-        for i in range (num-1):
-            resultado2=resultado2*resultado
-        if num<0:
-            resultado2 = 1/resultado2#no funciona
-        resultado=resultado2
+        resultado=potencia(resultado)
     if(selector==6):
-        print("")
-        raiz=int(input("Ingrese raíz: "))
-        while (raiz <= 0):
-            print("")
-            raiz=int(input("ERROR! Ingrese raíz válida: "))
-        if (raiz%2==0 and resultado<0):
-            print("ERROR! Esta cálculo es inválido matemáticamente")
-        else:
-            resultado = resultado ** (1/raiz)
+        resultado=raiz(resultado)
     print("")
     print(resultado)
 
